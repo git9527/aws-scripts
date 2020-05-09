c10d6e9d522eaebd0611ff6ee576190b389910f7
zhangsn <398816613@qq.com>
2020-05-09 11:36:24 +0800

    Update install-maven-and-gradle.sh

--- install-maven-and-gradle.sh
+++ install-maven-and-gradle.sh
@@ -5,6 +5,6 @@ unzip gradle-3.4.1-bin.zip
 wget http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
 tar -xvzf apache-maven-3.3.9-bin.tar.gz
 
-echo -e "export M2_HOME=/mnt/apache-maven-3.3.9" >> /etc/profile
-echo -e "export PATH=\${M2_HOME}/bin:\${PATH}" >> /etc/profile
-echo -e "export PATH=\$PATH:/mnt/gradle-3.4.1/bin" >> /etc/profile
+echo "export M2_HOME=/mnt/apache-maven-3.3.9" >> /etc/profile
+echo "export PATH=\${M2_HOME}/bin:\${PATH}" >> /etc/profile
+echo "export PATH=\$PATH:/mnt/gradle-3.4.1/bin" >> /etc/profile
